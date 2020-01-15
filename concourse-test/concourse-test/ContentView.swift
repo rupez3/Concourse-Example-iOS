a5407a672ab93f3451feaa4fce48519ac19b3ac4
Rupesh Chhetri <Rupesh_Chhetri@cable.comcast.com>
2020-01-14 22:04:50 -0500

    create a project

---
+++ concourse-test/concourse-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  concourse-test
+//
+//  Created by Rupesh Chhetri on 1/14/20.
+//  Copyright © 2020 Rupesh Chhetri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
